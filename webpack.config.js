bd5294ce84f76078061be0a68e112f62dc45e8dd
Wu <15151851516@163.com>
2022-04-26 00:07:26 +0800

    ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './lib/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'W-UI',
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  }
+};
