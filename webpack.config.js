f75bc99c589c6c5bbdb9ff3570e76864b975ca96
Wu <15151851516@163.com>
2022-04-26 00:33:58 +0800

    webpack-dev-server && html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
-  entry: './lib/index.tsx',
+  entry: {
+    index: './lib/index.tsx'
+  },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'W-UI',
     libraryTarget: "umd"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "W-UI",
+      template: "index.html"
+    })
+  ],
   module: {
     rules: [
       {
