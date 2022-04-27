d6f1cf013f4847f3a7ba56fc9958ce6689595829
Wu <15151851516@163.com>
2022-04-27 22:19:24 +0800

    icon 支持所有原生事件

--- lib/icon/index.tsx
+++ lib/icon/index.tsx
@@ -1,15 +1,16 @@
-import React, {FC, MouseEventHandler} from 'react';
+import React, {FC, SVGAttributes} from 'react';
 import './importAllIcons';
 import './icon.scss';
 
-interface Props {
+interface Props extends SVGAttributes<SVGSVGElement> {
   name: string;
-  onClick?: MouseEventHandler<SVGSVGElement>;
 }
 
-const Icon: FC<Props> = ({name, onClick}) => {
+const Icon: FC<Props> = ({name, className, ...restProps}) => {
   return (
-    <svg className="wui-icon" onClick={onClick}>
+    <svg className={`wui-icon ${className}`}
+         {...restProps}
+    >
       <use xlinkHref={`#${name}`}/>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Icon from './icon';
 
 ReactDOM.render(
-  <Icon onClick={()=>console.log('hi')} name="alipay"/>,
+  <Icon onClick={(e) =>
+    console.log((e.target as SVGUseElement).href)}
+        onMouseLeave={()=> console.log('leave')}
+        name="alipay"/>,
   document.getElementById('root')
 );
